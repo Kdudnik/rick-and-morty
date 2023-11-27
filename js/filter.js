054b4e6189cc8e807ce4f0b03e16fe08841261ab
Kiril Dudnik <kdudnik17@gmail.com>
2023-11-27 12:36:36 +0100

    refactor: filters js

--- js/filter.js
+++ js/filter.js
@@ -1,29 +1,37 @@
 import { foundedCharacters, charactersList } from "./main"
 import { generateCharacter } from "./utils/generateCharacter"
 
-const filters = document.querySelector('.filters-list')
-let activeFilter = null
+const filters = document.querySelector('.filters')
+let activeFilter = ''
 
 filters.addEventListener('click', event => {
     if(event.target.tagName !== "A") return
     charactersList.innerHTML = ""
+    filters.querySelectorAll('.filters__el').forEach(el => {
+        el.classList.remove('filter--active')
+    })
+    event.target.classList.add('filter--active')
 
-    // TODO: create a function to remove active class from all filters
-    // If filter is active, then remove it
-    if(event.target.dataset.filterActive === "true") {
-        event.target.dataset.filterActive = ""
+    setActiveFilter(event.target)
+    
+    foundedCharacters.forEach(character => {
+        let charactersStatus = character.status
+        if(charactersStatus.toLowerCase() == activeFilter.toLowerCase()) {
+            generateCharacter(character, charactersList)
+        }
+    })
+})
+
+const setActiveFilter = function(newActiveFilterEl) {
+    
+    if(newActiveFilterEl.innerHTML === activeFilter) {
+        activeFilter = ''
+        newActiveFilterEl.classList.remove('filter--active')
+        // regenerate all characters
         foundedCharacters.forEach(foundedCharacter => {
             generateCharacter(foundedCharacter, charactersList)
         })
         return
     }
-    activeFilter ? activeFilter.dataset.filterActive = "" : false
-    activeFilter = event.target
-    event.target.dataset.filterActive = "true"
-    foundedCharacters.forEach(character => {
-        let charactersStatus = character.status
-        if(charactersStatus.toLowerCase() == activeFilter.innerHTML.toLowerCase()) {
-            generateCharacter(character, charactersList)
-        }
-    })
-})
+    activeFilter = newActiveFilterEl.innerHTML
+}
